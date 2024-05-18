e5cf76778fd42d1f196cff095b2ec9854bcf1066
Iran2001 <lahiruhiripitiya18@gmail.com>
2024-05-18 22:43:43 +0530

    adding sever files

--- sever/src/index.js
+++ sever/src/index.js
@@ -1,2 +1,21 @@
-console.log("Hello world!");
-console.log("Hello world! Iran");
+const express=require('express');
+const mongooes=require('mongoose');
+
+const app=express();
+
+const PORT=8080;
+const DB_URL='mongodb+srv://admin:admin@capstonproject.rsoq76p.mongodb.net/?retryWrites=true&w=majority&appName=CapstonProject'
+
+mongooes.connect(DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).then(() => {
+    console.log('connected to database');
+}) .catch(err => console.log('DB connection error', err));
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+});
