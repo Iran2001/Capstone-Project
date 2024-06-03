680340d81f66296c5bf0008d287a4674f4d73c77
kavindyanimshani <134408710+Iran2001@users.noreply.github.com>
2024-06-03 09:54:59 +0530

    connect the data base and fix it.

--- sever/src/index.js
+++ sever/src/index.js
@@ -11,8 +11,47 @@ mongooes.connect(DB_URL,{
     useUnifiedTopology: true
 
 }).then(() => {
-    console.log('connected to database');
-}) .catch(err => console.log('DB connection error', err));
+    console.log('connected to database');const express = require("express");
+    const cors = require("cors");
+    const bodyParser = require("body-parser");
+    const db = require("./db");
+    
+    const app = express();
+    app.use(cors());
+    app.use(bodyParser.json());
+    
+    app.post("/api/signup", (req, res) => {
+        const { username, email, password } = req.body;
+    
+        const query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        db.query(query, [username, email, password], (err, results) => {
+            if (err) {
+                return res.status(500).send({ message: "Database error", error: err });
+            }
+            res.status(200).send({ message: "User registered successfully" });
+        });
+    });
+    
+    app.post("/api/signin", (req, res) => {
+        const { username, password } = req.body;
+    
+        const query = "SELECT * FROM users WHERE username = ? AND password = ?";
+        db.query(query, [username, password], (err, results) => {
+            if (err) {
+                return res.status(500).send({ message: "Database error", error: err });
+            }
+            if (results.length > 0) {
+                res.status(200).send({ message: "Login successful", user: results[0] });
+            } else {
+                res.status(401).send({ message: "Invalid credentials" });
+            }
+        });
+    });
+    
+    app.listen(5000, () => {
+        console.log("Server is running on port 5000");
+    });
+    }) .catch(err => console.log('DB connection error', err));
 
 
 
